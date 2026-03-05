2f0127b25992e1a6d6a1ab4ca6913bd6e401c0f0
paulopsms <paulopsms@gmail.com>
2026-03-05 20:35:46 -0300

    initial commit and project structure

---
+++ src/main/java/com/paulopsms/event_processing_engine/EventProcessingEngineApplication.java
@@ -0,0 +1,13 @@
+package com.paulopsms.event_processing_engine;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EventProcessingEngineApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EventProcessingEngineApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/paulopsms/event_processing_engine/EventProcessingEngineApplicationTests.java
@@ -0,0 +1,13 @@
+package com.paulopsms.event_processing_engine;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EventProcessingEngineApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
