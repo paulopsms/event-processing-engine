19b12833538968663647ede6ee050f39e36fd025
paulopsms <paulopsms@gmail.com>
2026-03-07 15:33:31 -0300

    Use Cases - Process Events, Process Batch, getSummaries and getEventIssues use cases

--- src/main/java/com/paulopsms/event_processing_engine/domain/factory/EventIssueFactory.java
+++ src/main/java/com/paulopsms/event_processing_engine/domain/factory/EventIssueFactory.java
@@ -5,16 +5,20 @@ import com.paulopsms.event_processing_engine.domain.enums.IssueType;
 import com.paulopsms.event_processing_engine.domain.model.EventIssue;
 import com.paulopsms.event_processing_engine.shared.exception.BusinessException;
 
+import static com.paulopsms.event_processing_engine.domain.enums.DeduplicationResult.CONFLICT;
 import static com.paulopsms.event_processing_engine.domain.enums.DeduplicationResult.DUPLICATE;
 
 public class EventIssueFactory {
 
-	public static EventIssue createEventIssue(String eventId, DeduplicationResult deduplicationResult) {
-		if (DUPLICATE.equals(deduplicationResult)) {
-			return createEventIssueDuplicate(eventId);
-		} else if (DeduplicationResult.CONFLICT.equals(deduplicationResult)) {
-			return createEventIssueConflict(eventId);
-		} else throw new BusinessException("Invalid Event Type found.");
+	public static EventIssue createEventIssue(String eventId, DeduplicationResult result) {
+		switch (result) {
+			case DUPLICATE:
+				return createEventIssueDuplicate(eventId);
+			case CONFLICT:
+				return createEventIssueConflict(eventId);
+			default:
+				throw new BusinessException("Invalid Event Type found.");
+		}
 	}
 
 	private static EventIssue createEventIssueDuplicate(String eventId) {
--- src/main/java/com/paulopsms/event_processing_engine/infrastructure/adapter/EventAdapter.java
+++ src/main/java/com/paulopsms/event_processing_engine/infrastructure/adapter/EventAdapter.java
@@ -6,6 +6,7 @@ import com.paulopsms.event_processing_engine.infrastructure.persistence.entity.E
 import com.paulopsms.event_processing_engine.infrastructure.persistence.mapper.EventMapper;
 import com.paulopsms.event_processing_engine.infrastructure.persistence.repository.EventJpaRepository;
 
+import java.util.List;
 import java.util.Optional;
 
 public class EventAdapter implements EventRepository {
@@ -28,5 +29,15 @@ public class EventAdapter implements EventRepository {
 		EventEntity eventEntity = this.eventMapper.toEntity(event);
 
 		this.eventJpaRepository.save(eventEntity);
+	}
+
+	@Override
+	public void deleteByEventId(String eventId) {
+		this.eventJpaRepository.deleteById(eventId);
+	}
+
+	@Override
+	public List<Event> findByAccountIdOrderByOcurredAt(String accountId) {
+		return this.eventJpaRepository.findByAccountIdOrderByOcurredAt(accountId);
 	}
 }
